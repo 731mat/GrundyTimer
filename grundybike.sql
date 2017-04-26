1671fd14be787b980644ff6ca5e9b2c2566a8062
731mat <matej.housek@seznam.cz>
2017-04-26 23:34:00 +0200

    add sql

--- grundybike.sql
+++ grundybike.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2017 at 11:21 PM
+-- Generation Time: Apr 26, 2017 at 11:33 PM
 -- Server version: 5.7.17-0ubuntu0.16.04.1
 -- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
@@ -13,6 +13,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `grundybike`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `count_round` int(11) NOT NULL,
+  `min_time` time NOT NULL DEFAULT '00:00:03'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `info`
+--
+
+CREATE TABLE `info` (
+  `name` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `date` date NOT NULL,
+  `adress` varchar(150) COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Dumping data for table `info`
+--
+
+INSERT INTO `info` (`name`, `date`, `adress`) VALUES
+('Grundy Bike', '2017-07-02', 'Grundy v neplachovicích');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `round`
+--
+
+CREATE TABLE `round` (
+  `id` int(11) NOT NULL,
+  `time` time NOT NULL,
+  `idUser` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -35,9 +79,48 @@ CREATE TABLE `user` (
   `dfn` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userRegistrated`
+--
+
+CREATE TABLE `userRegistrated` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) COLLATE utf8_czech_ci NOT NULL,
+  `password` varchar(150) COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Dumping data for table `userRegistrated`
+--
+
+INSERT INTO `userRegistrated` (`id`, `name`, `password`) VALUES
+(1, 'admin', 'admin');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `info`
+--
+ALTER TABLE `info`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `name` (`name`);
+
+--
+-- Indexes for table `round`
+--
+ALTER TABLE `round`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_C5EEEA34FE6E88D7` (`idUser`);
 
 --
 -- Indexes for table `user`
@@ -48,17 +131,44 @@ ALTER TABLE `user`
   ADD KEY `id` (`id`);
 
 --
+-- Indexes for table `userRegistrated`
+--
+ALTER TABLE `userRegistrated`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `round`
+--
+ALTER TABLE `round`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=675;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=554;
 --
+-- AUTO_INCREMENT for table `userRegistrated`
+--
+ALTER TABLE `userRegistrated`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `round`
+--
+ALTER TABLE `round`
+  ADD CONSTRAINT `FK_C5EEEA34FE6E88D7` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `user`
